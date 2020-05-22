1dfaaacd6f28624cd6635ecd0260933d50a9cb64
robertlin0401 <robertlin0401@gmail.com>
2020-05-23 02:05:43 +0800

    Revise: hw3/Q10150, use BFS algorithm, memory for input data not release, still TLE

--- hw3/Q10150.c
+++ hw3/Q10150.c
@@ -7,10 +7,11 @@
 
 struct doublet {
 	char *str;
+	int index;
 	struct doublet *next;
 };
 
-struct doublet *getResult(struct doublet *head, int doubletCount, char *endStr, int *isPickedConst, char *nowStr);
+struct doublet *getResult(struct doublet **list, int listIndex, int doubletCount, char *endStr, int *isPickedConst, char *nowStr);
 int isDoublet(char *str1, char *str2);
 void simplifyResult(struct doublet *result);
 void printResult(struct doublet *result);
@@ -21,14 +22,16 @@ int main() {
 	int i, j;	/* iterator */
 	int doubletCount = 0;
 	struct doublet *head = NULL;
+	struct doublet **list;
 	char *str = (char *)malloc(17 * sizeof(char));
 	
 	/* read doublet */
 	while (gets(str)) {
 		if (!str[0]) break;
-		struct doublet *newDoublet = (struct doublet *)malloc(sizeof(struct doublet));
+			struct doublet *newDoublet = (struct doublet *)malloc(sizeof(struct doublet));
 		newDoublet->str = (char *)malloc(17 * sizeof(char));
 		strcpy(newDoublet->str, str);
+		newDoublet->index = doubletCount;
 		newDoublet->next = NULL;
 		if (!head) {
 			head = newDoublet;
@@ -51,6 +54,47 @@ int main() {
 		}
 	*/
 	
+	/* generate adjacent list */
+	list = (struct doublet **)malloc(doubletCount * sizeof(struct doublet *));
+	struct doublet *ptrDoublet = head;
+	for (i = 0; i < doubletCount; ++i) {
+		struct doublet *ptrList = NULL;
+		struct doublet *ptrElement = head;
+		for (j = 0; j < doubletCount; ++j) {
+			if (i == j) {
+				ptrElement = ptrElement->next;
+				continue;
+			}
+			if (isDoublet(ptrDoublet->str, ptrElement->str)) {
+				struct doublet *element = (struct doublet *)malloc(sizeof(struct doublet));
+				element->str = ptrElement->str;
+				element->index = ptrElement->index;
+				element->next = NULL;
+				if (!ptrList) {
+					ptrList = element;
+					list[i] = ptrList;
+				} else {
+					ptrList->next = element;
+					ptrList = ptrList->next;
+				}
+			}
+			ptrElement = ptrElement->next;
+		}
+		ptrDoublet = ptrDoublet->next;
+	}
+	
+	/*
+		// print adjacent list
+		for (i = 0; i < doubletCount; ++i) {
+			struct doublet *ptr = list[i];
+			printf("%d\n", i);
+			while (ptr) {
+				printf("%s\n", ptr->str);
+				ptr = ptr->next;
+			}
+		}
+	*/
+	
 	/* test case */
 	int firstCase = 1;
 	int *isPicked = (int *)malloc(doubletCount * sizeof(int));
@@ -67,29 +111,30 @@ int main() {
 		str1 = strtok(str, " ");
 		str2 = strtok(NULL, " ");
 		
-		int count = 0;
 		struct doublet *result = (struct doublet *)malloc(sizeof(struct doublet));
 		struct doublet *ptr = head;
 		while (ptr) {
 			if (strcmp(ptr->str, str1) == 0) {
 				result->str = ptr->str;
+				result->index = ptr->index;
 				result->next = NULL;
 				break;
 			}
 			ptr = ptr->next;
-			++count;
 		}
 		if (!ptr) {
 			printf("No solution.\n");
 			continue;
 		}
 		
+		
 		for (i = 0; i < doubletCount; ++i) {
-			isPicked[i] = (i == count)? 1: 0;
+			isPicked[i] = (i == result->index)? 1: 0;
 		}
-		result->next = getResult(head, doubletCount, str2, isPicked, str1);
-		simplfyResult(result);
+		result->next = getResult(list, result->index, doubletCount, str2, isPicked, str1);
+		simplifyResult(result);
 		printResult(result);
+		
 		
 		while (result) {
 			struct doublet *temp = result->next;
@@ -105,7 +150,7 @@ int main() {
 	return 0;
 }
 
-struct doublet *getResult(struct doublet *head, int doubletCount, char *endStr, int *isPickedConst, char *nowStr) {
+struct doublet *getResult(struct doublet **list, int listIndex, int doubletCount, char *endStr, int *isPickedConst, char *nowStr) {
 	
 	if (strcmp(nowStr, endStr) == 0) {
 		return NULL;
@@ -117,25 +162,22 @@ struct doublet *getResult(struct doublet *head, int doubletCount, char *endStr, 
 		isPicked[iter] = isPickedConst[iter];
 	}
 	
-	int count = 0;
-	struct doublet *ptrDoublet = head;
+	struct doublet *ptrList = list[listIndex];
 	struct doublet *newResult = (struct doublet *)malloc(sizeof(struct doublet));
-	while (ptrDoublet) {
-		if (!isPicked[count]) {
-			if (isDoublet(ptrDoublet->str, nowStr)) {
-				isPicked[count] = 1;
-				newResult->str = ptrDoublet->str;
-				newResult->next = getResult(head, doubletCount, endStr, isPicked, newResult->str);
-				if (!newResult->next && strcmp(newResult->str, endStr) != 0) {
-					isPicked[count] = 0;
-				} else {
-					free(isPicked);
-					return newResult;
-				}
+	while (ptrList) {
+		if (!isPicked[ptrList->index]) {
+			isPicked[ptrList->index] = 1;
+			newResult->str = ptrList->str;
+			newResult->index = ptrList->index;
+			newResult->next = getResult(list, newResult->index, doubletCount, endStr, isPicked, newResult->str);
+			if (!newResult->next && strcmp(newResult->str, endStr) != 0) {
+				isPicked[ptrList->index] = 0;
+			} else {
+				free(isPicked);
+				return newResult;
 			}
 		}
-		ptrDoublet = ptrDoublet->next;
-		++count;
+		ptrList = ptrList->next;
 	}
 	
 	free(isPicked);
@@ -148,7 +190,13 @@ int isDoublet(char *str1, char *str2) {
 	int iter;
 	int count = 0;
 	for (iter = 0; iter < 17; ++iter) {
-		if (!str1[iter]) break;
+		if (!str1[iter] || !str2[iter]) {
+			if (!str1[iter] && !str2[iter]) {
+				break;
+			} else {
+				return 0;
+			}
+		}
 		if (str1[iter] != str2[iter]) {
 			count += 1;
 		}
